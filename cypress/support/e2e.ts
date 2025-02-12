bd048c96047703b8aecc015b8be3fed401065a14
nicholasjam <nicholashomme23@gmail.com>
2025-02-13 02:13:08 +0500

    refactor tsconfig file

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -3,7 +3,7 @@ import '@testing-library/cypress/add-commands'
 
 // Any additional custom commands can be added here
 
-Cypress.on('uncaught:exception', (err, runnable) => {
+Cypress.on('uncaught:exception', (_err, _runnable) => {
   // returning false here prevents Cypress from failing the test
   return false
 })
